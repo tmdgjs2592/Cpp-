8af7851099015ebb85e351817bb25a013e391c6f
John <tmdgjs2592@gmail.com>
2022-03-15 13:56:44 -0700

    created a function for chapter 2

--- SMC.cpp
+++ SMC.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void chapter2()
+{
+    int input;
+    int count = 1;
+    int max= 0;
+    int min = 0;
+    cout << "Input any number: ";
+    cin >> input;
+    while(input >=0){
+        cout << "Input another number: ";
+        cin >>input;
+        if (input >= max)
+        max = input;
+        if (input <= min)
+        min = input;
+        count ++;
+    }
+    cout << "Number of times entered: " << count << endl;
+    cout << "Max number entered: " << max << endl;
+    cout << "Minimum numebr entered :" << min << endl;
+}
+
 int main()
 {
-int input;
-int count = 1;
-int max= 0;
-int min = 0;
-cout << "Input any number: ";
-cin >> input;
-while(input >=0){
-    cout << "Input another number: ";
-    cin >>input;
-    if (input >= max)
-    max = input;
-    if (input <= min)
-    min = input;
-    count ++;
-}
-cout << "Number of times entered: " << count << endl;
-cout << "Max number entered: " << max << endl;
-cout << "Minimum numebr entered :" << min << endl;
-return 0;
+    chapter2();
+    return 0;
 }
